67a44db6a59996c347cc783460040df332d35b37
Eric Horodyski <horodyski@ionic.io>
2023-04-05 09:58:52 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'fw7-react-tea-taster',
+  webDir: 'dist',
+  bundledWebRuntime: false,
+};
+
+export default config;
